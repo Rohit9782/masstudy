9b72c651b809461feb82c869ef4e43420d998596
Rohit9782 <rohitkumarsharma3440@gmail.com>
2026-02-27 15:05:17 +0530

    Update registration API URL to production

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -25,7 +25,7 @@ const Register = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/user/register",
+        "https://masstudy.onrender.com/user/register",
         {
           name: formData.name,
           email: formData.email,
